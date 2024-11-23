b019a77099d4de09ba4a8b8238bac060921f4f7d
GoodBro <104461617+ChrisGoodBrother@users.noreply.github.com>
2024-11-23 23:46:01 +0100

    Camera and Level Design
    -Added camera movement, making the camera follow the player as well as not go off borders (from the left).
    -Made simple level design for starters

---
+++ Assets/Scripts/CameraFollower.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class CameraFollower : MonoBehaviour
+{
+    private Transform player;
+
+    private void Awake() {
+        player = GameObject.FindWithTag("Player").transform;
+   }
+
+   private void Update() {
+        Vector3 cameraPosition = transform.position;
+        cameraPosition.x = player.position.x;
+        if(!(cameraPosition.x <= -9))
+            transform.position = cameraPosition; 
+   }
+}
