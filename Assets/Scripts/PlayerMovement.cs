0051c5539f2fb74d02e444e9c1b8f5551f8a336e
papoutsistavroula@gmail.com <papoutsistavroula@gmail.com>
2024-12-29 01:36:21 +0200

    player goes to level2-problem with jump movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,7 +9,10 @@ public class PlayerMovement : MonoBehaviour
     private Animator animator;
     private BoxCollider2D boxCollider2D;
 
-    private void Awake() {
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject); // Ο παίκτης δεν καταστρέφεται κατά την αλλαγή σκηνής
+
         playerBody = GetComponent<Rigidbody2D>();
         playerSpeed = 5f;
         playerJumpHeight = 14.5f;
@@ -17,30 +20,34 @@ public class PlayerMovement : MonoBehaviour
         boxCollider2D = GetComponent<BoxCollider2D>();
     }
 
-    private void Update() {
+    private void Update()
+    {
 
         //Move player left or right according to the input
-        float horizontalMovement = Input.GetAxis("Horizontal"); 
+        float horizontalMovement = Input.GetAxis("Horizontal");
         playerBody.velocity = new Vector2(horizontalMovement * playerSpeed, playerBody.velocity.y);
 
         //Flip Player Model
-        if(horizontalMovement >= 0.01f)
+        if (horizontalMovement >= 0.01f)
             transform.localScale = Vector3.one;
-        else if(horizontalMovement <= - 0.01f)
-            transform.localScale = new Vector3(-1,1,1);
+        else if (horizontalMovement <= -0.01f)
+            transform.localScale = new Vector3(-1, 1, 1);
 
         //Make player jump if player is on the ground and not already jumping
-        if(Input.GetKey(KeyCode.Space) && isGrounded()) {
+        if (Input.GetKey(KeyCode.Space) && isGrounded())
+        {
             Jumping();
         }
 
         //Make player run if ShiftKey is pressed
-        if(Input.GetKey(KeyCode.LeftShift) && horizontalMovement != 0) {
+        if (Input.GetKey(KeyCode.LeftShift) && horizontalMovement != 0)
+        {
             playerBody.velocity = new Vector2(horizontalMovement * playerSpeed * 1.5f, playerBody.velocity.y); //Move player left and right but faster
             animator.SetBool("walk", false);
             animator.SetBool("run", true);
-        } 
-        else {
+        }
+        else
+        {
             animator.SetBool("run", false);
             animator.SetBool("walk", horizontalMovement != 0);
         }
@@ -49,15 +56,19 @@ public class PlayerMovement : MonoBehaviour
     }
 
     //Make player jump
-    private void Jumping() {
+    private void Jumping()
+    {
         playerBody.velocity = new Vector2(playerBody.velocity.x, playerJumpHeight);
         animator.SetTrigger("jump");
     }
 
     //Check if player is on the ground and not in the air
-    private bool isGrounded() {
+    private bool isGrounded()
+    {
         RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider2D.bounds.center, boxCollider2D.bounds.size, 0, Vector2.down, 0.1f, groundLayer);
 
         return raycastHit.collider != null;
     }
 }
+
+
