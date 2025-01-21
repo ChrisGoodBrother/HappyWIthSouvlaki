3ac79ce3062c62d7ea2e82cb9af97d635c87e633
GoodBro <104461617+ChrisGoodBrother@users.noreply.github.com>
2025-01-22 00:50:11 +0100

    Bug fix after merge

--- Assets/Scripts/EnemyStats.cs
+++ Assets/Scripts/EnemyStats.cs
@@ -7,10 +7,24 @@ public class EnemyStats : MonoBehaviour
     [SerializeField] public float maxHealth;
     [SerializeField] private float currentHealth;
     [SerializeField] private ParticleSystem explosionEffect;
+    [SerializeField] private Image foregroundImage;
+    private HealthBarController healthBarController;
+    private bool isAlive;
+
+    public bool getIsAlive() {
+        return isAlive;
+    }
 
     private void Awake() {
-        maxHealth = 300f;
-        currentHealth = 10f;
+        maxHealth = 100f;
+        currentHealth = 100f;
+        isAlive = true;
+        healthBarController = GameObject.FindGameObjectWithTag("GameController").GetComponent<HealthBarController>();
+    }
+
+    private void Start() {
+        healthBarController.setEnemyHealthBarActive(true);
+        healthBarController.UpdateHealthBar(currentHealth, maxHealth, "enemy");
     }
 
     //Damage Player
@@ -18,7 +32,7 @@ public class EnemyStats : MonoBehaviour
     public void take_damage(float damage)
     {
         currentHealth -= damage;
-        if (currentHealth <= 0) {
+        if (currentHealth < 0) {
             if (explosionEffect != null) // Έλεγχος για το εφέ της έκρηξης
             {
                 Instantiate(explosionEffect, transform.position + new Vector3(0, 0, -1), Quaternion.identity);
@@ -26,6 +40,7 @@ public class EnemyStats : MonoBehaviour
 
             Destroy(gameObject);
         }
+        
+    healthBarController.UpdateHealthBar(currentHealth, maxHealth, "enemy");
     }
 }
-
