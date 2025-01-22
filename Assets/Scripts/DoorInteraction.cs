67d772fddae67d66ce2af6e81f5719afe055afcd
GoodBro <104461617+ChrisGoodBrother@users.noreply.github.com>
2025-01-22 03:38:00 +0100

    ExtraPreparation

--- Assets/Scripts/DoorInteraction.cs
+++ Assets/Scripts/DoorInteraction.cs
@@ -4,22 +4,35 @@ using UnityEngine.SceneManagement;
 
 public class DoorInteraction : MonoBehaviour
 {
-    public string targetScene = "Level_2"; // Το όνομα της σκηνής
-    public Vector3 spawnPointPosition = new Vector3(-10, 0, 0); // Σημείο εκκίνησης στο Level 2
+    private string winScene = "WinScene";
+    private string level2Scene = "Level_2"; // Το όνομα της σκηνής
+    private Vector3 spawnPointPosition = new Vector3(-10, 0, 0); // Σημείο εκκίνησης
+    private string sceneName;
+
+    void Update() {
+        Scene currentScene = SceneManager.GetActiveScene();
+        sceneName = currentScene.name;
+    }
 
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
-            DontDestroyOnLoad(other.gameObject); // Διατήρηση του παίκτη
-            SceneManager.sceneLoaded += OnSceneLoaded; // Προσθήκη event για μετακίνηση
-            SceneManager.LoadScene(targetScene); // Φόρτωση νέας σκηνής
+            if(sceneName == "Level_1") {
+                DontDestroyOnLoad(other.gameObject); // Διατήρηση του παίκτη
+                SceneManager.sceneLoaded += OnSceneLoaded; // Προσθήκη event για μετακίνηση
+                SceneManager.LoadScene(level2Scene); // Φόρτωση νέας σκηνής
+            }
+            //else if(sceneName == "Level_2") {
+            //    SceneManager.sceneLoaded += OnSceneLoaded; // Προσθήκη event για μετακίνηση
+            //    SceneManager.LoadScene(winScene); // Φόρτωση νέας σκηνής
+            //}
         }
     }
 
     void OnSceneLoaded(Scene scene, LoadSceneMode mode)
     {
-        if (scene.name == targetScene)
+        if (scene.name == level2Scene)
         {
             GameObject player = GameObject.FindWithTag("Player");
             if (player != null)
